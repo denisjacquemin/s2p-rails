1e5979d960e7cb5069e3d4c7b5788b7ea9ed6753
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-02-25 22:04:53 +0100

    adding foreign key from user to school

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209171602) do
+ActiveRecord::Schema.define(version: 20160225165926) do
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160209171602) do
     t.string   "invited_by_type"
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
+    t.integer  "school_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
