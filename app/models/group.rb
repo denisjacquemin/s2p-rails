b2767b5b0939dc88ef6492188ea1553881d4e384
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-02 23:56:22 +0100

    adding scopes to 'Group' model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,9 @@
 class Group < ApplicationRecord
-  belongs_to :school
+  belongs_to :school, required: false
 
+  scope :by_ids, ->(ids) { where(id: ids) }
+  scope :by_student_id, ->(student_id) { where("? = ANY(students)", student_id) }
+  scope :by_school, ->(school_id) { where(school_id: school_id) }
 
   def students
     Student.by_group_id(self.id)
