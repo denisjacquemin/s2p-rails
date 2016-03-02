f6f3f6235791fd7fa005da857c4221949d4a8164
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-02 13:48:22 +0100

    adding 'groups' array in student table

--- db/migrate/20160226120735_create_students.rb
+++ db/migrate/20160226120735_create_students.rb
@@ -3,7 +3,10 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.string :firstname
       t.string :lastname
-
+      # see http://edgeguides.rubyonrails.org/active_record_postgresql.html#array
+      # see http://blog.plataformatec.com.br/2014/07/rails-4-and-postgresql-arrays/
+      # see http://stackoverflow.com/questions/24236871/in-rails-how-to-add-an-element-to-an-array-type-attribute-for-all-records
+      t.integer :groups, array: true, default: []
       t.timestamps
     end
   end
