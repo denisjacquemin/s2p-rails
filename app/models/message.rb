bdb852d85acc576a2945233ab04a25d0c33cd1f8
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-03 11:43:13 +0100

    adding useful methods to 'Message' model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,19 @@
 class Message < ApplicationRecord
+  belongs_to :school, required: false
+
+  def groups_obj
+    Group.by_ids(self.groups)
+  end
+
+  scope :by_ids, ->(ids) { where(id: ids) }
+
+  def self.add_groups(message_ids, group_ids)
+    Message.by_ids(message_ids).update_all(['groups = array_cat(groups, ARRAY[?])', group_ids])
+  end
+
+  def self.remove_groups(message_ids, group_ids)
+    group_ids.each do |g_id|
+      Message.by_ids(message_ids).update_all(['groups = array_remove(groups, ?)', g_id])
+    end
+  end
 end
