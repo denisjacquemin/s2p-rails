cf9d1c88d831cebb3487a4b60d9f4345941111f9
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-17 13:27:17 +0100

    dont update if array is empty

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -74,7 +74,7 @@ class MessagesController < ApplicationController
     # check if submited groups are not yet in db
     submitted_groups_to_add = submitted_groups_ids.select { |g| actual_groups_ids.include?(g) }
 
-    Message.add_groups(@message.id, submitted_groups_to_add.map(&:to_i)) if submitted_groups_ids.any?
+    Message.add_groups(@message.id, submitted_groups_to_add.map(&:to_i)) if submitted_groups_to_add.any?
     Message.remove_groups(@message.id, actual_groups_to_delete) if actual_groups_to_delete.any?
     redirect_to messages_url, notice: 'Message was successfully updated.'
   end
