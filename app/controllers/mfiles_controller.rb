bd5bb1321545880b38b9bdf83872b25d6c981637
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-30 14:44:20 +0200

    redirect to same screen after delete

--- app/controllers/mfiles_controller.rb
+++ app/controllers/mfiles_controller.rb
@@ -52,10 +52,7 @@ class MfilesController < ApplicationController
   # DELETE /mfiles/1.json
   def destroy
     @mfile.destroy
-    respond_to do |format|
-      format.html { redirect_to mfiles_url, notice: 'Mfile was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to edit_message_path(@mfile.message, t: 'files'), notice: 'Le message a été mis à jour.'
   end
 
   private
