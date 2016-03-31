00bef236426cbe8b5fd705d91ae288e7a0ee2a44
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-31 17:41:52 +0200

    adding deleted_at column in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330085352) do
+ActiveRecord::Schema.define(version: 20160331115512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20160330085352) do
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
     t.integer  "school_id"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
