01153e1070449ea1299a93580dfb52f6e5050ac0
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-05-09 19:28:10 +0200

    adding Device

---
+++ db/migrate/20160509172347_create_devices.rb
@@ -0,0 +1,10 @@
+class CreateDevices < ActiveRecord::Migration[5.0]
+  def change
+    create_table :devices do |t|
+      t.string, :token
+      t.integer :groups, array: true, default: []
+
+      t.timestamps
+    end
+  end
+end
