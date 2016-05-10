3666c11a5546a7dbd4bf20df73e17d6be8ec2ebe
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-05-10 11:09:33 +0200

    remove comma

--- db/migrate/20160509172347_create_devices.rb
+++ db/migrate/20160509172347_create_devices.rb
@@ -1,7 +1,7 @@
 class CreateDevices < ActiveRecord::Migration[5.0]
   def change
     create_table :devices do |t|
-      t.string, :token
+      t.string :token
       t.integer :groups, array: true, default: []
 
       t.timestamps
