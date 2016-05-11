d11ccb542d2f5a332d31722777f3717e6ebf8fc5
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-05-11 14:17:26 +0200

    array of integer instead of string

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,7 @@ class Student < ApplicationRecord
 
   scope :by_ids, ->(ids) { where(id: ids) }
   scope :by_group, ->(id) { where("? = ANY(groups)", id) }
-  scope :by_groups, ->(ids) { where("groups && ARRAY[?]::varchar[]", ids) }
+  scope :by_groups, ->(ids) { where("groups && ARRAY[?]::integer[]", ids) }
   scope :by_school, ->(school_id) { where(school_id: school_id) }
   scope :by_code, ->(code) { where(code: code) }
 
