6ec401083cbb2fa8426e0d079e21f81657ce54e3
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-08 15:38:18 +0200

    fix notification_platform is not an array

--- db/migrate/20160608115045_add_platform_to_device.rb
+++ db/migrate/20160608115045_add_platform_to_device.rb
@@ -1,6 +1,6 @@
 class AddPlatformToDevice < ActiveRecord::Migration[5.0]
   def change
     add_column :devices, :platform, :string
-    add_column :devices, :notification_platform, :integer, array: true, default: []
+    add_column :devices, :notification_platform, :integer
   end
 end
