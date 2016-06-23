98b63399990d6c1ee1c3ccc1dfcae50d2b725b81
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-23 14:38:51 +0200

    fix logs

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -126,7 +126,7 @@ class MessagesController < ApplicationController
                            icon: 'myicon'
                          }
         n.save!
-        puts "notification Google: " n.inspect
+        puts "notification Google: #{n.inspect}" 
       }
 
       redirect_to messages_url, notice: 'Message publié avec succès'
