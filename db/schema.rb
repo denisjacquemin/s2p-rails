c5ca80963c7e41ebcb551b324d046d9055dcd17e
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-23 15:48:53 +0200

    add url to School

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617132043) do
+ActiveRecord::Schema.define(version: 20160623134729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20160617132043) do
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.boolean  "validation_workflow_active", default: true
+    t.string   "url"
   end
 
   create_table "students", force: :cascade do |t|
