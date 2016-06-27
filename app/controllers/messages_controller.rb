4765d523fb4128505c53273ec59fe9ed03b19d20
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-06-27 10:37:05 +0200

    test badge for ios notification

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -105,7 +105,7 @@ class MessagesController < ApplicationController
         n.data = {
           "title": truncate(@message.title, :length => 200),
           "message_id": @message.id,
-          "count": 1
+          "badge": 1
         }
         begin
           n.save!
