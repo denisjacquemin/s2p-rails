1e7b24113e92b43a457687fce820478942676356
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-07-06 13:49:34 +0200

    brand name Konecto

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 's2p@example.com'
+  config.mailer_sender = 'konecto@konectoapp.be'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
