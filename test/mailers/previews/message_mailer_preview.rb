494bff5434113e83d07e1a7e6ab994a9b978055e
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-08-05 21:23:44 +0200

    adding test for message_email and improving message layout

--- test/mailers/previews/message_mailer_preview.rb
+++ test/mailers/previews/message_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/message_mailer
 class MessageMailerPreview < ActionMailer::Preview
 
+  def message_email
+    # /rails/mailers/message_mailer/message_email.
+    MessageMailer.message_email('me@example.com', Message.find(32))
+  end
 end
