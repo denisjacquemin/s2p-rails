71a7d5c6aa875ae1c56ac32e5a0515d2000612a2
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-08-17 17:22:38 +0200

    remove notification for workflow

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -194,18 +194,18 @@ class MessagesController < ApplicationController
     authorize @message
     @message.waiting_for_approval!
     # send notification to admins
-    codes = @message.school.admins.map{|u| u.code}
-
-    title = "#{@message.author.firstname} demande une approbation"
-    content = @message.title
-
-    @message.title = title
-    @message.content = content
-
-    devicesIOS = Device.active.ios.by_codes(codes)
-    build_ios_notifications(@message, devicesIOS) unless devicesIOS.nil?
-    devicesAndroid = Device.active.android.by_codes(codes)
-    build_android_notifications(@message, devicesAndroid) unless devicesAndroid.nil?
+    # codes = @message.school.admins.map{|u| u.code}
+    #
+    # title = "#{@message.author.firstname} demande une approbation"
+    # content = @message.title
+    #
+    # @message.title = title
+    # @message.content = content
+    #
+    # devicesIOS = Device.active.ios.by_codes(codes)
+    # build_ios_notifications(@message, devicesIOS) unless devicesIOS.nil?
+    # devicesAndroid = Device.active.android.by_codes(codes)
+    # build_android_notifications(@message, devicesAndroid) unless devicesAndroid.nil?
 
     if @message.save
       redirect_back fallback_location: messages_url, notice: 'Message envoyé pour approbation avec succès'
@@ -218,18 +218,18 @@ class MessagesController < ApplicationController
     authorize @message
     @message.approval_accepted!
 
-    codes = [] <<  @message.author.code
-
-    title = "Message approuvé"
-    content = @message.title
-
-    @message.title = title
-    @message.content = content
-
-    devicesIOS = Device.active.ios.by_codes(codes)
-    build_ios_notifications(@message, devicesIOS) unless devicesIOS.nil?
-    devicesAndroid = Device.active.android.by_codes(codes)
-    build_android_notifications(@message, devicesAndroid) unless devicesAndroid.nil?
+    # codes = [] <<  @message.author.code
+    #
+    # title = "Message approuvé"
+    # content = @message.title
+    #
+    # @message.title = title
+    # @message.content = content
+    #
+    # devicesIOS = Device.active.ios.by_codes(codes)
+    # build_ios_notifications(@message, devicesIOS) unless devicesIOS.nil?
+    # devicesAndroid = Device.active.android.by_codes(codes)
+    # build_android_notifications(@message, devicesAndroid) unless devicesAndroid.nil?
 
 
 
