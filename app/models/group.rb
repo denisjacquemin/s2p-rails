8ff77b5227af686fa8c4bf9e8b3b3616f241245f
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-09-17 06:11:16 +0200

    adding by_code scope on Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,6 +10,7 @@ class Group < ApplicationRecord
   scope :by_school, ->(school_id) { where(school_id: school_id) }
   scope :all_writers_by_schools, ->(school_ids) { where(internal_id: 'all_writers', school_id: school_ids) }
   scope :all_students_by_school, ->(school_id) { where(internal_id: 'all_students', school_id: school_id) }
+  scope :by_code, ->(code) { where(code: code) }
 
   default_scope { order('name ASC') }
 
