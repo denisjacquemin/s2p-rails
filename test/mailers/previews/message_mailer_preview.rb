2cf6eeef36e974511e09ee6ff2cd42025cf54b0f
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-10-08 05:44:16 +0200

    email simplified

--- test/mailers/previews/message_mailer_preview.rb
+++ test/mailers/previews/message_mailer_preview.rb
@@ -3,6 +3,6 @@ class MessageMailerPreview < ActionMailer::Preview
 
   def message_email
     # /rails/mailers/message_mailer/message_email
-    MessageMailer.message_email('me@example.com', Message.find(32))
+    MessageMailer.message_email('me@example.com', Message.find(6))
   end
 end
