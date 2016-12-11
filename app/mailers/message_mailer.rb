c12e6e0e2fa9ba2ac8991e189b36ac617e74c904
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-12-11 18:14:18 +0100

    message mailer refactored

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -17,12 +17,44 @@ class MessageMailer < ApplicationMailer
     # <% #      <p><%= image_tag attachments[@message.mfiles[0].filename].url %></p><!-- /hero -->
     # <% #end %>
     # <!--<td><%= image_tag attachments['logo.png'].url %></td>-->
-    headers "X-SMTPAPI" => {
-      unique_args: { mid: @message.id, sid: @message.school_id }
-    }.to_json
+
+    x_smptapi_hash = {}
+    x_smptapi_hash['unique_args'] = { mid: @message.id, sid: @message.school_id }
+    x_smptapi_hash['to'] = to if to.kind_of?(Array) # if to argument is an array build 'to' X-SMTPAPI list
+    x_smptapi_hash['to'] = [to] if to.kind_of?(String)
+
+    codes = []
+    if message.content.include?('[code]')
+      # "sub": {
+      #   "[code]": [
+      #     "John",
+      #     "Jane"
+      #   ]
+      # }
+      to.each do |email|
+        # for current email gets all students to build the codes
+        students_containing_email_string = Student.where("emails LIKE ?", "%#{email}%").by_school(@message.school_id)
+        students = students_containing_email_string.select do |s|
+          emails = s.emails.split(' ').collect(&:strip);
+          emails.include?(email)
+        end
+
+        codes << students.map do |student|
+          "<li>#{student.fullname}: #{student.code}</li>"
+        end.join || ""
+      end
+    end
+    
+    unless codes.blank?
+      x_smptapi_hash['sub'] = {
+        "[code]": codes
+      }
+    end
+    headers "X-SMTPAPI" => x_smptapi_hash.to_json
 
     from = "#{@message.school_name} #{@message.author.fullname}" + '<' + 'konecto@konectoapp.com' + '>' || 'konecto@konectoapp.com'
     reply_to = @message.author.fullname + '<' + @message.author.email + '>' || 'konecto@konectoapp.com'
-    mail(from: from, to: to, subject: @message.title, reply_to: reply_to )
+    resp = mail(from: from, to: 'konecto@konectoapp.com', subject: @message.title, reply_to: reply_to )
+    logger.info "message_email response: #{resp.inspect}"
   end
 end
