5bc3bd2cda5b2ce4c256173017ca75fb17a72da2
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-03-30 14:48:29 +0200

    fix constant error

--- app/models/concerns/notification.rb
+++ app/models/concerns/notification.rb
@@ -37,9 +37,9 @@ module Notification extend ActiveSupport::Concern
         ios_badges: "+1"
       }
       other_options = {}
-      PWdevices = devicesAndroid + devicesIOS
-      logger.info "devicesAndroid + devicesIOS: #{PWdevices.inspect}"
-      resp = Pushwoosh.notify_devices("PW: #{message.title}", PWdevices, other_options)
+      pwdevices = devicesAndroid + devicesIOS
+      logger.info "devicesAndroid + devicesIOS: #{pwdevices.inspect}"
+      resp = Pushwoosh.notify_devices("PW: #{message.title}", pwdevices, other_options)
       logger.info "after Pushwoosh.notify_devices: #{resp.inspect}"
     end
 
