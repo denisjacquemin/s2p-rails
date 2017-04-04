5a21f61a4a8c4b0b7d7146fa95b8fa281c3163e2
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-04-04 16:46:24 +0200

    remove send_notification_with_pushwoosh

--- app/models/message.rb
+++ app/models/message.rb
@@ -103,8 +103,6 @@ class Message < ApplicationRecord
   def handle_publish
     self.publish_date = DateTime.now
     groups = self.groups
-    logger.info "[PW] before send_message_notifications #{self.inspect}"
-    send_notification_with_pushwoosh(self)
     if groups.present? or self.students.present?
 
       send_message_notifications(self) if self.send_to_app
