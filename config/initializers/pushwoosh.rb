4f0b9fdd3e9aec18b02a67536051797304a97f7c
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-04-09 14:43:44 +0200

    remove pushwoosh initializer

--- config/initializers/pushwoosh.rb
+++ config/initializers/pushwoosh.rb
@@ -1,9 +1,9 @@
-require 'pushwoosh/pushwoosh_sync'
-require 'pushwoosh'
-Pushwoosh.configure do |config|
-  config.application = ENV["PUSHWOOSH_APPLICATION_CODE"]
-  config.auth = ENV["PUSHWOOSH_API_TOKEN"]
-end
+# require 'pushwoosh/pushwoosh_sync'
+# require 'pushwoosh'
+# Pushwoosh.configure do |config|
+#   config.application = ENV["PUSHWOOSH_APPLICATION_CODE"]
+#   config.auth = ENV["PUSHWOOSH_API_TOKEN"]
+# end
 # include Pushwoosh
 # Pushwoosh.configure do |config|
 #   config.application = ENV["PUSHWOOSH_APPLICATION_CODE"]
