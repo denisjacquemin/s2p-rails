974a217b2d22788a98fd8c0efbb954ed12b45674
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-04-20 11:20:23 +0200

    add missing "

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -232,7 +232,7 @@ class StudentsController < ApplicationController
 
   def export_csv
     students = Student.where(:id => params[:s])
-    send_data(students.to_csv_file.encode("cp1252),
+    send_data(students.to_csv_file.encode("cp1252"),
       type: 'text/csv; charset=iso-8859-1; header=present',
       disposition: 'attachment',
       filename: "eleves-#{current_school.name.parameterize}-#{Date.today}.csv")
