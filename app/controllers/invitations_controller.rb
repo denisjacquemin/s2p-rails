cb0431ec238c57b22a9a18a107efdd228e2b0355
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-05-08 11:46:33 +0200

    redirect to /users after an invitation is sent

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,26 @@
 class InvitationsController < Devise::InvitationsController
 
+  def create
+    self.resource = invite_resource
+    resource_invited = resource.errors.empty?
+
+    yield resource if block_given?
+
+    if resource_invited
+      if is_flashing_format? && self.resource.invitation_sent_at
+
+        set_flash_message :notice, :send_instructions, :email => self.resource.email
+      end
+      if self.method(:after_invite_path_for).arity == 1
+        respond_with resource, :location => after_invite_path_for(current_inviter)
+      else
+        respond_with resource, :location => users_path
+      end
+    else
+      respond_with_navigational(resource) { render :new }
+    end
+  end
+
   private
     def invite_resource
         resource_class.invite!(invite_params, current_inviter) do |invitable|
