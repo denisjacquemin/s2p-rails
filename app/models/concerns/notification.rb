4da2686f131f0c37671e20a0b509c2e2dda0bf31
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-06-02 11:57:13 +0200

    remove setting of expiry parameter

--- app/models/concerns/notification.rb
+++ app/models/concerns/notification.rb
@@ -98,7 +98,6 @@ module Notification extend ActiveSupport::Concern
           n.alert = truncate(alert, :length => 256)
           n.content_available = true
           n.badge = 1
-          n.expiry = 86400
           n.sound = true
           n.data = data
 
