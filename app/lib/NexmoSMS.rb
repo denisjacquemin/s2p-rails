b16d53938260d59abf3ab8422ed8280c877734d9
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-07-03 14:17:56 +0200

    send sms is back

--- app/lib/NexmoSMS.rb
+++ app/lib/NexmoSMS.rb
@@ -6,7 +6,7 @@ module NexmoSMS
   end
 
   def self.sendMessages(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
-    logger.info "Sending SMS to (#{to.inspect}), message is #{message}"
+    logger.info "(sendMessages) Sending SMS to (#{to.inspect}), message is #{message}"
     client = Nexmo::Client.new(key: Rails.application.secrets.nexmo_key, secret: Rails.application.secrets.nexmo_secret)
 
     @message = '[KonectoApp] ' + message
