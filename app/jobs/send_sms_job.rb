8e1c30e149217d42bcd24c7b0d785322c1b494ae
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-07-03 15:23:28 +0200

    logger fix in Nexmo Module

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -2,6 +2,7 @@ class SendSmsJob < ApplicationJob
   queue_as :default
 
   def perform(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
+    byebug
     logger.info "In SendSmsJob #{message} #{to.inspect}"
     NexmoSMS::sendMessages(message, to, from, type, delivery_receipt)
   end
--- app/lib/NexmoSMS.rb
+++ app/lib/NexmoSMS.rb
@@ -6,7 +6,7 @@ module NexmoSMS
   end
 
   def self.sendMessages(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
-    logger.info "(sendMessages) Sending SMS to (#{to.inspect}), message is #{message}"
+    Rails.logger.info "(sendMessages) Sending SMS to (#{to.inspect}), message is #{message}"
     client = Nexmo::Client.new(key: Rails.application.secrets.nexmo_key, secret: Rails.application.secrets.nexmo_secret)
 
     @message = '[KonectoApp] ' + message
@@ -14,9 +14,9 @@ module NexmoSMS
     to.compact.uniq.each do |number|
       response = client.send_message(from: from, to: number, text: @message[0...160])
       if response['messages'][0]['status'] == '0'
-        logger.info "Sent message #{response['messages'][0]['message-id']}"
+        Rails.logger.info "Sent message #{response['messages'][0]['message-id']}"
       else
-        logger.info "Error: #{response['messages'][0]['error-text']}"
+        Rails.logger.info "Error: #{response['messages'][0]['error-text']}"
       end
     end
   end
