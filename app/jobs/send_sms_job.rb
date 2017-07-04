b722fa09fe93f8605151e0e7359dcf937673268b
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-07-04 13:12:17 +0200

    prefix logs with [SMS]

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -2,11 +2,11 @@ class SendSmsJob < ApplicationJob
   queue_as :default
 
   def perform(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
-    logger.info "In SendSmsJob #{message} #{to.inspect}"
+    logger.info "[SMS] In SendSmsJob #{message} #{to.inspect}"
     begin
       NexmoSMS::sendMessages(message, to, from, type, delivery_receipt)
     rescue Exception => e
-      logger.info "Error: #{e.inspect}"
+      logger.info "[SMS] Error: #{e.inspect}"
     end
   end
 end
--- app/lib/NexmoSMS.rb
+++ app/lib/NexmoSMS.rb
@@ -7,14 +7,14 @@ module NexmoSMS
 
   def self.sendMessages(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
     # begin
-      Rails.logger.info "####### In NexmoSMS::sendMessages"
-      Rails.logger.info "ENV['NEXMO_KEY']: #{ENV['NEXMO_KEY']}"
-      Rails.logger.info "ENV['NEXMO_SECRET']: #{ENV['NEXMO_SECRET']}"
-      Rails.logger.info "Rails.application.secrets.nexmo_key: #{Rails.application.secrets.nexmo_key}"
-      Rails.logger.info "Rails.application.secrets.nexmo_secret: #{Rails.application.secrets.nexmo_secret}"
+      Rails.logger.info "[SMS] ####### In NexmoSMS::sendMessages"
+      Rails.logger.info "[SMS] ENV['NEXMO_KEY']: #{ENV['NEXMO_KEY']}"
+      Rails.logger.info "[SMS] ENV['NEXMO_SECRET']: #{ENV['NEXMO_SECRET']}"
+      Rails.logger.info "[SMS] Rails.application.secrets.nexmo_key: #{Rails.application.secrets.nexmo_key}"
+      Rails.logger.info "[SMS] Rails.application.secrets.nexmo_secret: #{Rails.application.secrets.nexmo_secret}"
 
 
-      Rails.logger.info "(sendMessages [#{ENV["NEXMO_KEY"]}]) Sending SMS to (#{to.inspect}), message is #{message}"
+      Rails.logger.info "([SMS] sendMessages [#{ENV["NEXMO_KEY"]}]) Sending SMS to (#{to.inspect}), message is #{message}"
       # client = Nexmo::Client.new(key: ENV["NEXMO_KEY"], secret: ENV["NEXMO_SECRET"])
       #
       # @message = '[KonectoApp] ' + message
