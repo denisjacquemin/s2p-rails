448bc3c44e5470dc6be8ed2eaaaa407aae392174
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-07-04 17:44:25 +0200

    removing nexmo calls

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -9,20 +9,20 @@ class SendSmsJob < ApplicationJob
 private
   def sendMessagesNexmo(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
       logger.info "([SMS] sendMessages [#{ENV["NEXMO_KEY"]}]) Sending SMS to (#{to.inspect}), message is #{message}"
-      client = Nexmo::Client.new(key: ENV["NEXMO_KEY"], secret: ENV["NEXMO_SECRET"])
+      #client = Nexmo::Client.new(key: ENV["NEXMO_KEY"], secret: ENV["NEXMO_SECRET"])
 
       @message = '[KonectoApp] ' + message
 
       logger.info "to: #{to.compact.uniq.inspect}"
       to.compact.uniq.each do |number|
         logger.info "[SMS] sending to #{number} with client: #{client.inspect}"
-        response = client.send_message(from: from, to: number, text: @message[0...160])
-        logger.info "[SMS] response: #{response.inspect}"
-        if response['messages'][0]['status'] == '0'
-          logger.info "[SMS] Sent message #{response['messages'][0]['message-id']}"
-        else
-          logger.info "[SMS] Error: #{response['messages'][0]['error-text']}"
-        end
+        #response = client.send_message(from: from, to: number, text: @message[0...160])
+        # logger.info "[SMS] response: #{response.inspect}"
+        # if response['messages'][0]['status'] == '0'
+        #   logger.info "[SMS] Sent message #{response['messages'][0]['message-id']}"
+        # else
+        #   logger.info "[SMS] Error: #{response['messages'][0]['error-text']}"
+        # end
       end
   end
 end
