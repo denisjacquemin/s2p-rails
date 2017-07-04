04f9072c72989d91ebae23fc7e780f7e1bf6c9c0
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-07-04 09:20:07 +0200

    keeps logs only

--- app/lib/NexmoSMS.rb
+++ app/lib/NexmoSMS.rb
@@ -6,7 +6,7 @@ module NexmoSMS
   end
 
   def self.sendMessages(message, to=[], from="KonectoApp", type="text", delivery_receipt=true)
-    begin
+    # begin
       Rails.logger.info "####### In NexmoSMS::sendMessages"
       Rails.logger.info "ENV['NEXMO_KEY']: #{ENV['NEXMO_KEY']}"
       Rails.logger.info "ENV['NEXMO_SECRET']: #{ENV['NEXMO_SECRET']}"
@@ -15,20 +15,20 @@ module NexmoSMS
 
 
       Rails.logger.info "(sendMessages [#{ENV["NEXMO_KEY"]}]) Sending SMS to (#{to.inspect}), message is #{message}"
-      client = Nexmo::Client.new(key: ENV["NEXMO_KEY"], secret: ENV["NEXMO_SECRET"])
-
-      @message = '[KonectoApp] ' + message
-
-      to.compact.uniq.each do |number|
-        response = client.send_message(from: from, to: number, text: @message[0...160])
-        if response['messages'][0]['status'] == '0'
-          Rails.logger.info "Sent message #{response['messages'][0]['message-id']}"
-        else
-          Rails.logger.info "Error: #{response['messages'][0]['error-text']}"
-        end
-      end
-    rescue Exception => e
-      Rails.logger.info "Error in NexmoSMS::sendMessages: #{e.inspect}"
-    end
+      # client = Nexmo::Client.new(key: ENV["NEXMO_KEY"], secret: ENV["NEXMO_SECRET"])
+      #
+      # @message = '[KonectoApp] ' + message
+      #
+      # to.compact.uniq.each do |number|
+      #   response = client.send_message(from: from, to: number, text: @message[0...160])
+      #   if response['messages'][0]['status'] == '0'
+      #     Rails.logger.info "Sent message #{response['messages'][0]['message-id']}"
+      #   else
+      #     Rails.logger.info "Error: #{response['messages'][0]['error-text']}"
+      #   end
+      # end
+    # rescue Exception => e
+    #   Rails.logger.info "Error in NexmoSMS::sendMessages: #{e.inspect}"
+    # end
   end
 end
