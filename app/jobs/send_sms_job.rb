ef2bddc5cec22106f705f5bb700af1b817fc0154
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-07-04 19:45:18 +0200

    moving loop outside private method

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -3,7 +3,9 @@ class SendSmsJob < ApplicationJob
 
   def perform(message, to)
     logger.info "[SMS] In SendSmsJob #{message} #{to.inspect}"
-    sendMessagesNexmo(message, to)
+    to.each do |number|
+      sendMessagesNexmo(message, to)
+    end
   end
 
   rescue_from(StandardError) do |exception|
@@ -11,22 +13,19 @@ class SendSmsJob < ApplicationJob
   end
 
 private
-  def sendMessagesNexmo(message, to)#, from="KonectoApp", type="text", delivery_receipt=true)
-      logger.info "([SMS] sendMessages [#{ENV["NEXMO_KEY"]}]) Sending SMS to (#{to.inspect}), message is #{message}"
+  def sendMessagesNexmo(message, number)#, from="KonectoApp", type="text", delivery_receipt=true)
+      logger.info "([SMS] sendMessages [#{ENV["NEXMO_KEY"]}]) Sending SMS to (#{number}), message is #{message}"
       #client = Nexmo::Client.new(key: ENV["NEXMO_KEY"], secret: ENV["NEXMO_SECRET"])
 
       @message = '[KonectoApp] ' + message
-      toArray = to.compact.uniq
-      logger.info "[SMS] to: #{to.compact.uniq.inspect}"
-      toArray.each do |number|
-        logger.info "[SMS] sending to #{number} with client: #{client.inspect}"
-        #response = client.send_message(from: from, to: number, text: @message[0...160])
-        # logger.info "[SMS] response: #{response.inspect}"
-        # if response['messages'][0]['status'] == '0'
-        #   logger.info "[SMS] Sent message #{response['messages'][0]['message-id']}"
-        # else
-        #   logger.info "[SMS] Error: #{response['messages'][0]['error-text']}"
-        # end
+
+      logger.info "[SMS] sending to #{number} with client: #{client.inspect}"
+      response = client.send_message(from: from, to: number, text: @message[0...160])
+      logger.info "[SMS] response: #{response.inspect}"
+      if response['messages'][0]['status'] == '0'
+        logger.info "[SMS] Sent message #{response['messages'][0]['message-id']}"
+      else
+        logger.info "[SMS] Error: #{response['messages'][0]['error-text']}"
       end
   end
 end
