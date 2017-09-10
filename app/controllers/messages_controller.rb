2640a1e114c615f22493bbd0345855f8a7809f65
Denis Jacquemin <denis.jacquemin@gmail.com>
2017-09-10 08:23:00 +0200

    no authentication required for refresh_qr

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :save_form]
+  before_action :authenticate_user!, except: [:show, :save_form, :refresh_qr]
   before_action :set_message, only: [:edit, :update, :update_amount_to_pay, :publish, :unpublish, :republish, :send_for_approval, :accept, :reject, :update_groups, :destroy, :add_photo, :update_formdata, :export_formdata]
   before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
