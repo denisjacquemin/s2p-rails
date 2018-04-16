c8d4b86e05bada5fe4796df9224ed71d1006ea2e
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-04-16 14:49:43 +0200

    removing byebug

--- app/mailers/alert_admin_mailer.rb
+++ app/mailers/alert_admin_mailer.rb
@@ -1,7 +1,6 @@
 class AlertAdminMailer < ApplicationMailer
 
   def send_alert(msg)
-    byebug
     @msg = msg
     mail(from: "S2P App", to: 'denis.jacquemin@gmail.com', subject: "Alert S2P" )
   end
