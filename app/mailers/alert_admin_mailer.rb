278cad3b8857dd83ca3fd894bf6a212e128ea114
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-04-16 15:02:02 +0200

    from param fix

--- app/mailers/alert_admin_mailer.rb
+++ app/mailers/alert_admin_mailer.rb
@@ -2,7 +2,7 @@ class AlertAdminMailer < ApplicationMailer
 
   def send_alert(msg)
     @msg = msg
-    mail(from: "S2P App", to: 'denis.jacquemin@gmail.com', subject: "Alert S2P" )
+    mail(to: 'denis.jacquemin@gmail.com', subject: "Alert S2P" )
   end
 
 end
