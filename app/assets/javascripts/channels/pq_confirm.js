07190cbd1035588c53601e83a11d0ab6faae51ac
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-04-24 14:46:42 +0200

    show notification when payment is done

--- app/assets/javascripts/channels/pq_confirm.js
+++ app/assets/javascripts/channels/pq_confirm.js
@@ -11,8 +11,8 @@ App.pq_confirm = App.cable.subscriptions.create("PqConfirmChannel", {
     // Called when there's incoming data on the websocket for this channel
     $('#pq-ids input').each(function (index, input){
       if (input.value === data.pq_transaction_id) {
-        alert('payment done' + data.pq_transaction_id)
-      } else (alert('nope' + data.pq_transaction_id))
+        $('#pq-notification').html('<div class="alert alert-success animated bounceIn">Votre paiment a bien été enregistré.</div>')
+      }
     });
   }
 });
