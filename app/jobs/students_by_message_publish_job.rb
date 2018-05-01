5650076daf36cef6ac3c7521567b2df38ea50cff
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-05-01 16:40:53 +0200

    removing byebug

--- app/jobs/students_by_message_publish_job.rb
+++ app/jobs/students_by_message_publish_job.rb
@@ -6,7 +6,6 @@ class StudentsByMessagePublishJob < ApplicationJob
       students = student_ids.map do |id|
         {student_id: id, message_id: message_id, school_id: school_id}
       end
-      byebug
       StudentRecipient.create(students)
     end
   end
