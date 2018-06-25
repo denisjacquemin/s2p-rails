5f93d09fec4b06ccb0cd50b6ef59d4917d780f58
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-06-25 09:58:50 +0200

    adding lastname in emails notifiactions

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,9 @@
 class NotificationMailer < ApplicationMailer
 
-  def approval_requested(emails, message_title, author_firstname)
+  def approval_requested(emails, message_title, author_firstname, author_lastname)
     @message_title = message_title
     @author_firstname = author_firstname
+    @author_lastname = author_lastname
     mail(to: emails, subject: "[Konecto] #{author_firstname} demande une approbation: #{message_title[0..50]}")
   end
 end
