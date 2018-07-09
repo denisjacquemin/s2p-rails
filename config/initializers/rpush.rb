2281d1f4db4cff16990735e7d91cf8d324aee099
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-07-09 13:33:32 +0200

    handle rpush gcm_canonical_id event

--- config/initializers/rpush.rb
+++ config/initializers/rpush.rb
@@ -119,6 +119,8 @@ Rpush.reflect do |on|
   # You will need to replace old_id with canonical_id in your records.
   on.gcm_canonical_id do |old_id, canonical_id|
     Rails.logger.info "[Rpush.reflect gcm_canonical_id] old_id: #{old_id}, canonical_id: #{canonical_id}"
+    d = Device.find_by(registration_id: old_id)
+    d.update_attributes(registration_id: canonical_id)
   end
 
   # Called when the GCM returns a failure that indicates an invalid registration id.
