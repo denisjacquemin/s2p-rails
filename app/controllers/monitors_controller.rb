4bd664b8c16d4d760a0c0d4394271b1bff83be45
Denis Jacquemin <denis.jacquemin@gmail.com>
2018-08-14 13:39:17 +0200

    adding latest messages

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -5,6 +5,7 @@ class MonitorsController < ApplicationController
   def index
     if current_user.superadmin?
        @latest_logged_users = User.order('last_sign_in_at IS NULL, last_sign_in_at DESC').limit(15)
+       @latest_messages = Message.order('created_at IS NULL, created_at DESC').limit(15))
     else
        redirect_to root_url # or whatever
     end
