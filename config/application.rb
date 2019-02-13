d224fa4b794a25a6925adda272f00d3fd716c862
Denis Jacquemin <denisjacquemin@MacBook-Pro-de-Denis.local>
2019-02-13 11:13:58 +0100

    fix Rack::QueryParser::InvalidParameterError (invalid %-encoding

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,20 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require_relative "../app/middleware/handle_bad_encoding_middleware"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 require "google/cloud/translate"
 
+
+
 module School2parents
   class Application < Rails::Application
+
+    config.middleware.use HandleBadEncodingMiddleware
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
