a3badfd129755c763cc011c77269a4097f8a0a84
Denis Jacquemin <denis.jacquemin@gmail.com>
2019-06-07 13:56:35 +0200

    testing no-reply email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -67,13 +67,20 @@ class MessageMailer < ApplicationMailer
     #   }
     # end
     headers "X-SMTPAPI" => JSON.generate(x_smptapi_hash)
+    no_reply = "no-reply@konectoapp.com"
 
     from = %Q["#{@message.school_name}"] + '<' + 'konecto@konectoapp.com' + '>' || 'konecto@konectoapp.com'
+
+    if [10, 67].include? @message.school_id 
+      from = %Q["#{@message.school_name}"] + '<' + no_reply + '>' || no_reply
+    end
 
     reply_to = 'konecto@konectoapp.com'
     if @message.author.display_email_address
       reply_to = @message.author.fullname + '<' + @message.author.reply_to + '>' || 'konecto@konectoapp.com'
     end
+
+    
     resp = mail(from: from, to: 'konecto@konectoapp.com', subject: title, reply_to: reply_to )
     logger.info "message_email response: #{resp.inspect}"
   end
