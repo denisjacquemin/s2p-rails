6b0a6d9a8503d7389d2f55b6ab996c08e20d02a1
Denis Jacquemin <denis.jacquemin@gmail.com>
2019-08-23 16:13:09 +0200

    fix missing all_students group

--- app/jobs/create_student_from_csv_v2_job.rb
+++ app/jobs/create_student_from_csv_v2_job.rb
@@ -75,6 +75,9 @@ class CreateStudentFromCsvV2Job < ApplicationJob
 
         # build new groups if required and gets all group ids
         student_data[:groups] = []
+        # get all_students group
+        group_all_students = Group.all_students_by_school(school_id).first
+        student_data[:groups].push(group_all_students.id) unless group_all_students.nil?
         [ student_data[:level], student_data[:classroom], data[:group1], data[:group2], data[:group3], data[:group4], data[:group5], data[:group6], data[:group7], data[:group8], data[:group9], data[:group10] ].compact.each do |group_name|
             group_id = Group.find_or_create_group(group_name, student_data[:school_id]).id
             student_data[:groups].push(group_id)
