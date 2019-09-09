528826bf4a3a35c5bbc1ac89af9ca089fcaf9863
Denis Jacquemin <denis.jacquemin@gmail.com>
2019-09-09 12:15:51 +0200

    trim email before save

--- app/models/student_email.rb
+++ app/models/student_email.rb
@@ -1,3 +1,7 @@
 class StudentEmail < ApplicationRecord
   belongs_to :student, inverse_of: :student_emails
+
+  before_validation do
+    self.email = self.email.strip unless self.email.blank?
+  end
 end
