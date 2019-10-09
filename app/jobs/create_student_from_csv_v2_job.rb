111f2611ac4ca239057a4d7b69588ff5473f95f5
Denis Jacquemin <denis.jacquemin@gmail.com>
2019-10-09 11:19:27 +0200

    removing byebug

--- app/jobs/create_student_from_csv_v2_job.rb
+++ app/jobs/create_student_from_csv_v2_job.rb
@@ -14,7 +14,6 @@ class CreateStudentFromCsvV2Job < ApplicationJob
 
     def perform(rows, school_id, user)
         rows.each do |data|
-            byebug
             if data[:firstname].present? and data[:lastname].present? # check if mandatory fields are presents
                 create_or_update_student(data, school_id, user.id)
             end
