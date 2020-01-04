2238a2ac3e2efef03d1b847b3d88f4232624128f
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-01-04 14:14:55 +0100

    adding log in job

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -2,7 +2,9 @@ class SendMailJob < ApplicationJob
   queue_as :default
 
   def perform(message_id)
+    puts "[SendMailJob info: #{message_id}]"
     message = Message.find message_id
+
     unless message.nil?
       message.status = message.status == 'published' ? 'republished' : 'published'
       message.scheduled_publish = nil
