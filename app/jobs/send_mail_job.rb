279f8b9876caef63320b8fccc21db10c4f291206
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-01-04 17:20:22 +0100

    keep only the log

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -1,19 +1,21 @@
 class SendMailJob < ApplicationJob
   queue_as :default
 
-  def perform(message_id)
-    puts "[SendMailJob info: #{message_id}]"
-    message = Message.find message_id
-    ret = true
-    unless message.nil?
-      message.status = message.status == 'published' ? 'republished' : 'published'
-      message.scheduled_publish = nil
-      ret =  message.save
-    end
-    return ret
-  end
-
   rescue_from(Exception) do |exception|
     puts "Exception in SendMailJob: #{exception.inspect}"
-   end
+  end
+
+  def perform(message_id)
+    puts "[SendMailJob info: #{message_id}]"
+    # message = Message.find message_id
+    # ret = true
+    # unless message.nil?
+    #   message.status = message.status == 'published' ? 'republished' : 'published'
+    #   message.scheduled_publish = nil
+    #   ret =  message.save
+    # end
+    # return ret
+  end
+
+  
 end
