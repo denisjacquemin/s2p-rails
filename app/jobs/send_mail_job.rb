69a2c7c0107e17687218b8c03a4099d8dce27e3b
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-01-04 16:18:21 +0100

    return save result as job result

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -4,11 +4,12 @@ class SendMailJob < ApplicationJob
   def perform(message_id)
     puts "[SendMailJob info: #{message_id}]"
     message = Message.find message_id
-    byebug
+    ret = true
     unless message.nil?
       message.status = message.status == 'published' ? 'republished' : 'published'
       message.scheduled_publish = nil
-      message.save
+      ret =  message.save
     end
+    return ret
   end
 end
