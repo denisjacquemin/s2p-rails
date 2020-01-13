ba0d6374c6a544366f7092543c97e9e61b1a132f
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-01-13 15:55:21 +0100

    handle publish

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -2,7 +2,7 @@ class SendMailJob < ApplicationJob
   queue_as :default
 
   rescue_from(Exception) do |exception|
-    puts "Exception in SendMailJob: #{exception.inspect}"
+    # puts "Exception in SendMailJob: #{exception.inspect}"
     AlertAdminMailer.send_alert("Exception in SendMailJob: #{exception.inspect}").deliver_later
   end
 
@@ -11,13 +11,13 @@ class SendMailJob < ApplicationJob
 
     message = Message.find message_id
     # puts "[SendMailJob message: #{message.inspect}"
-    # ret = true
-    # unless message.nil?
-    #   message.status = message.status == 'published' ? 'republished' : 'published'
-    #   message.scheduled_publish = nil
-    #   ret =  message.save
-    # end
-    # return ret
+    ret = true
+    unless message.nil?
+      message.status = message.status == 'published' ? 'republished' : 'published'
+      message.scheduled_publish = nil
+      ret =  message.save
+    end
+    return ret
   end
 
   
