3c2342754bc6bbf826d94f2981f43f49eec43709
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-02-10 14:44:36 +0100

    send scheduled message

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -7,17 +7,16 @@ class SendMailJob < ApplicationJob
   end
 
   def perform(message_id)
-    AlertAdminMailer.send_alert("Start SendMailJob for: #{message_id}").deliver_later
 
-    # message = Message.find message_id
-    # # puts "[SendMailJob message: #{message.inspect}"
-    # ret = true
-    # unless message.nil?
-    #   message.status = message.status == 'published' ? 'republished' : 'published'
-    #   message.scheduled_publish = nil
-    #   ret =  message.save
-    # end
-    # return ret
+    message = Message.find message_id
+    # puts "[SendMailJob message: #{message.inspect}"
+    ret = true
+    unless message.nil?
+      message.status = message.status == 'published' ? 'republished' : 'published'
+      message.scheduled_publish = nil
+      ret =  message.save
+    end
+    return ret
   end
 
   
