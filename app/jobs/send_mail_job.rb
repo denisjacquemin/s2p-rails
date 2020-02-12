238a00eb8f18e57810da37dc35c57bb0c391ebbe
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-02-12 09:20:08 +0100

    add log in job

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -1,22 +1,25 @@
 class SendMailJob < ApplicationJob
   queue_as :SendMailJob
 
+
+
+  def perform(message_id)
+    puts "[SendMailJob info] processing: #{message_id}"
+
+    # message = Message.find message_id
+    # # puts "[SendMailJob message: #{message.inspect}"
+    # ret = true
+    # unless message.nil?
+    #   message.status = message.status == 'published' ? 'republished' : 'published'
+    #   message.scheduled_publish = nil
+    #   ret =  message.save
+    # end
+    # return ret
+  end
+
   rescue_from(Exception) do |exception|
     # puts "Exception in SendMailJob: #{exception.inspect}"
     AlertAdminMailer.send_alert("Exception in SendMailJob: #{exception.inspect}").deliver_later
-  end
-
-  def perform(message_id)
-
-    message = Message.find message_id
-    # puts "[SendMailJob message: #{message.inspect}"
-    ret = true
-    unless message.nil?
-      message.status = message.status == 'published' ? 'republished' : 'published'
-      message.scheduled_publish = nil
-      ret =  message.save
-    end
-    return ret
   end
 
   
