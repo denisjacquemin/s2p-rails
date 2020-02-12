d542e51bc2d38863769a9bcc196f64267b88a7ab
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-02-12 11:29:42 +0100

    adding delayed job initializer

--- app/jobs/send_mail_job.rb
+++ app/jobs/send_mail_job.rb
@@ -22,9 +22,5 @@ class SendMailJob < ApplicationJob
     AlertAdminMailer.send_alert("Exception in SendMailJob: #{exception.inspect}").deliver_later
   end
 
-  def destroy_failed_jobs?
-    false
-  end
-
   
 end
