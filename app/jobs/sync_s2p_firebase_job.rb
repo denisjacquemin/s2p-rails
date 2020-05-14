2fdf27176a7fea0679c8175e304e93d62922db7a
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-05-14 10:22:26 +0200

    adding sync message job

---
+++ app/jobs/sync_s2p_firebase_job.rb
@@ -0,0 +1,39 @@
+
+class SyncS2pFirebaseJob < ApplicationJob
+  queue_as :syncS2pFirebase
+
+  def perform(message)
+
+    studend_ids = Message.find_student_ids(message.groups, message.students, message.school.iscity, message.message_categories)
+    if !studend_ids.empty?
+
+      files = message.photos.map do |photo|
+        photo.path
+      end
+
+      data = { 
+        'data': {
+          'id': message.id,
+          'title': message.title,
+          'content': message.content,
+          'students': studend_ids,
+          'files': files,
+          'form': message.formdata,
+          'muuid': message.muuid
+        }   
+      }.to_json
+
+      begin
+        uri = URI(ENV['S2P_FIREBASE_HOST'] + '/addMessage')
+        response = Faraday.post do |req|
+          req.url uri
+          req.headers['Content-Type'] = "application/json; charset=utf-8"
+          req.headers['Authorization'] = "Bearer #{ENV['S2P_USERID_FIREBASE_CLOUDFUNCTIONS']}"
+          req.body = data
+        end
+      rescue => e
+          puts "failed #{e}"
+      end
+    end
+  end
+end
