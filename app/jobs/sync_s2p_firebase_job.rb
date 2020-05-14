3e505dbbcf584f16f0f7f0a6dbcd31cf9463b379
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-05-14 11:48:56 +0200

    store fullpath for files

--- app/jobs/sync_s2p_firebase_job.rb
+++ app/jobs/sync_s2p_firebase_job.rb
@@ -8,7 +8,7 @@ class SyncS2pFirebaseJob < ApplicationJob
     if !studend_ids.empty?
 
       files = message.photos.map do |photo|
-        { format: photo.format, path: photo.path }
+        { format: photo.format, path: photo.fullpath }
       end
 
       data = { 
