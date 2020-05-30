23c9fea909aa3a125e710223fd2f696abf5a43bb
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-05-30 16:02:26 +0200

    disable csrf check waiting to fix the random issue, this bas I know

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend: true
 
+  skip_forgery_protection
+
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
