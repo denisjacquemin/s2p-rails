3a1cdf9cb781d405590ac90cf6e2e9f0b27ec591
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-09-04 16:18:52 +0200

    improve mobile menu clickabilty

--- app/assets/javascripts/responsive_tabs.js
+++ app/assets/javascripts/responsive_tabs.js
@@ -4,10 +4,10 @@ $.fn.responsiveTabs = function() {
     this.append($('<span class="glyphicon glyphicon-triangle-top"></span>'));
 
     this.on('click', 'li.active > a, span.glyphicon', function(event) {
-        $(event.target.parentElement).toggleClass('open');
+        $(event.target).closest('ul').toggleClass('open');
     }.bind(this));
 
-    this.on('click', 'li:not(.active) > a', function() {
+    this.on('click', 'li:not(.active) > a', function(event) {
         this.removeClass('open');
     }.bind(this));
 };
