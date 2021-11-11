10e7a3936cbcc1e5740f70d3fb9799a437bd9f20
Denis Jacquemin <denis.jacquemin@gmail.com>
2021-11-11 17:33:06 +0100

    fix issue with homonyms from ProEco

--- app/jobs/create_student_from_csv_v2_job.rb
+++ app/jobs/create_student_from_csv_v2_job.rb
@@ -129,7 +129,6 @@ class CreateStudentFromCsvV2Job < ApplicationJob
 
     def get_already_existing_student_for_update(student_data, school_id, user_id)
         student = nil
-
         if student_data[:proeco_id].present?
             student = Student.where('proeco_id = ? and school_id = ?', student_data[:proeco_id].to_s, school_id).first
         end
@@ -143,7 +142,7 @@ class CreateStudentFromCsvV2Job < ApplicationJob
         end
 
         # student not found based on proeco_id/winpage_matricule, try to find it by firstname and lastname
-        if student.nil?
+        if not student_data[:proeco_id].present? and student.nil?
             student = Student.where('lower(firstname) = ? and lower(lastname) = ? and school_id = ?', student_data[:firstname].downcase.strip, student_data[:lastname].downcase.strip, school_id).first
             # if students.size == 1
             # student = students.first
