057e612d345b20e4dd76abdbb11c4d6b81ec6ae6
Denis Jacquemin <denis.jacquemin@gmail.com>
2021-11-30 14:16:15 +0100

    add logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,9 @@ class ApplicationController < ActionController::Base
   end
 
   def custom_headers
+    puts "$$$$$$$$$$$$ IN custom_headers $$$$$$$$$$$$"
     headers['TEST2'] = 'this is a test'
     headers['Public-Key-Pins'] = 'pin-sha256="base64=="; max-age=expireTime [; includeSubDomains][; report-uri="reportURI"]'
+    puts "responses.headers.inspect #{responses.headers.inspect}"
   end
 end
