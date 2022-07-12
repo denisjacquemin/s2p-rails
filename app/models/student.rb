8a555b79b1297cc41b4d56f3dbd87a1242ad220d
Denis Jacquemin <denis.jacquemin@gmail.com>
2022-07-12 17:38:46 +0200

    dealing with nils

--- app/models/student.rb
+++ app/models/student.rb
@@ -228,7 +228,7 @@ class Student < ApplicationRecord
             phone4,
             code,
             followed ? 'Oui': 'Non'
-          ].concat(emails.split(' ').select{|e| e.include?('@')})
+          ].concat(emails&.split(' ')&.select{|e| e.include?('@')} || [])
         end
       end
     end
