f2f84813914ca4c87d95fc249a0bf08cfe922e87
Denis Jacquemin <denis.jacquemin@gmail.com>
2023-05-26 10:16:00 +0200

    test on platform name case insensitive because of cordova capacitor migration

--- app/models/device.rb
+++ app/models/device.rb
@@ -4,9 +4,9 @@ class Device < ApplicationRecord
 
   scope :by_codes, ->(codes) { where("codes && ARRAY[?]::varchar[]", codes) }
   scope :active, -> { where(active: true) }
-  scope :android, -> { where(platform: 'Android') }
-  scope :ios, -> { where(platform: 'iOS') }
-
+  scope :android, -> { where("LOWER(platform) = ?", 'android') }
+  scope :ios, -> { where("LOWER(platform) = ?", 'ios') }
+  
   def disable
     self.update(active: false)
   end
