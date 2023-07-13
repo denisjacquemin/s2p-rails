5fbd41ccc442f09a26dd15916dec32e4b1e20cc1
Denis Jacquemin <denis.jacquemin@gmail.com>
2023-07-13 10:29:13 +0200

    adding column proeco5 :année_classe_classe=>

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -388,6 +388,7 @@ class StudentsController < ApplicationController
             :email_adresse_principale_mère_élève => :email3,
             :libellé_classe => :implantation, # ProEco 5
             :groupe_classe => :level1, # ProEco 5
+            :année_classe_classe=> :level1,
             
             # gestscol
             #             :nom => :lastname,
